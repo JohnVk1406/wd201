a4ca4b35b82dc006677f0b31ba059670763f2858
Johan Varghese K <jv1406k@gmail.com>
2025-06-16 06:25:54 +0000

    tests added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,3 +8,17 @@ export default defineConfig([
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
+{
+  "env": {
+    "commonjs": true,
+    "es2021": true,
+    "node": true,
+    "jest": true
+  },
+  "extends": "eslint:recommended",
+  "overrides": [],
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {}
+};
